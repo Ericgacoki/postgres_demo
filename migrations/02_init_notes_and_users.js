16df0cab7d1931cc7ed67eeadc1646e37ee86ac6
Ericgacoki <gacokieric@gmail.com>
2025-09-26 10:09:01 +0300

    add db migration file

---
+++ migrations/02_init_notes_and_users.js
@@ -0,0 +1,55 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    // Create users first
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    })
+
+    // Then notes
+    await queryInterface.createTable('notes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      important: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }
+    })
+  },
+
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('notes')
+    await queryInterface.dropTable('users')
+  }
+}
