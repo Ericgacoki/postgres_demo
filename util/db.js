a84e21aa19fa7dab0df6979bdb7bebd7e1e2c13d
Ericgacoki <gacokieric@gmail.com>
2025-09-30 21:05:55 +0300

    add rollback migrations

--- util/db.js
+++ util/db.js
@@ -6,21 +6,6 @@ const sequelize = new Sequelize(DATABASE_URL)
 
 console.log('URL', DATABASE_URL)
 
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: 'migrations/*.js',
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  })
-
-  const migrations = await migrator.up()
-  console.log('Migrations up to date', {
-    files: migrations.map((mig) => mig.name),
-  })
-}
 
 const connectToDatabase = async () => {
   try {
@@ -35,4 +20,27 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { connectToDatabase, sequelize }
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+  
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
